159e1ac022bb065c5fcc615375fbc3a3b0cc74e8
ThoMarJos <thomarjos@gmail.com>
2026-01-07 10:54:04 +0530

    Made some changes to treesitter.lua as per some reddit suggestions

--- lua/ak_config/plugins/treesitter.lua
+++ lua/ak_config/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = function()
         local ts = require("nvim-treesitter")
-
-        ts.install({
+        local parsers = {
             "bash",
             "powershell",
             "c",
@@ -23,6 +23,29 @@ return {
             "javascript",
             "typescript",
             "tsx",
+        }
+
+        vim.defer_fn(function()
+            ts.install(parsers)
+        end, 1000)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("ak-treesitter-features", {}),
+            callback = function(args)
+                local buf = args.buf
+                local filetype = args.match
+
+                local language = vim.treesitter.language.get_lang(filetype) or filetype
+                if not vim.treesitter.language.add(language) then
+                    return
+                end
+
+                vim.treesitter.start(buf, language)
+
+                vim.bo[buf].indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+                vim.wo.foldmethod = "expr"
+                vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            end,
         })
     end,
 }
